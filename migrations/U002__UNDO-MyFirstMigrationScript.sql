555d8b95a13aceefb804cb8d5014461487ab02a3
chris.hawkins@red-gate.com <chris.hawkins@red-gate.com>
2024-09-17 22:15:05 +0100

    Updated Flyway.toml to include references to all Widget databses as Environments. Consolidated migration scripts into one simple V002.

--- migrations/U002__UNDO-MyFirstMigrationScript.sql
+++ migrations/U002__UNDO-MyFirstMigrationScript.sql
@@ -2,8 +2,16 @@
 GO
 SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
 GO
+PRINT N'Dropping [dbo].[CurrentPrices]'
+GO
+DROP VIEW [dbo].[CurrentPrices]
+GO
 PRINT N'Dropping [dbo].[GetAllWidgets]'
 GO
 DROP PROCEDURE [dbo].[GetAllWidgets]
+GO
+PRINT N'Altering [dbo].[WidgetPrices]'
+GO
+ALTER TABLE [dbo].[WidgetPrices] ALTER COLUMN [Price] [decimal] (19, 4) NULL
 GO
 
--- migrations/V002__MyFirstMigrationScript.sql
+++ migrations/V002__MyFirstMigrationScript.sql
@@ -2,6 +2,21 @@
 GO
 SET ANSI_PADDING, ANSI_WARNINGS, CONCAT_NULL_YIELDS_NULL, ARITHABORT, QUOTED_IDENTIFIER, ANSI_NULLS ON
 GO
+PRINT N'Creating [dbo].[GetAllWidgets]'
+GO
+
+CREATE   PROCEDURE [dbo].[GetAllWidgets]
+AS
+BEGIN
+	SELECT RecordID,
+           Description 
+	FROM Widgets
+END
+GO
+PRINT N'Altering [dbo].[WidgetPrices]'
+GO
+ALTER TABLE [dbo].[WidgetPrices] ALTER COLUMN [Price] [decimal] (18, 0) NULL
+GO
 PRINT N'Creating [dbo].[CurrentPrices]'
 GO
 
